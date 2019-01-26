52c55f7595e7de73332d75ca0e81cb8caf1fabaa
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 04:51:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,17 @@ def line(currentLineArr)
   if currentLineArr.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + currentLineArr.each_with_index {|person, i| "#{i + 1}. #{person} "}
+    beginningMsg = "The line is currently: "
+    
+    i = 0
+    while i < currentLineArr.length
+      currentPerson = currentLineArr[i]
+      beginningMsg += "#{i + 1}. #{currentPerson} "
+      
+    i += 1
+    end
+    
+    puts beginningMsg
   end
   
 end
