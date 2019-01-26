4c5104558dd382d4c14f41f42201dec35a9de444
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 05:04:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,7 +30,8 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody in waiting to be served!"
-  end
-  
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   
 end
