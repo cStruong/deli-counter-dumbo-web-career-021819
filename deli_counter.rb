1a872328374c4bbd03b2c3eae105cd6bedde7738
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 04:34:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(currentLineArr)
   
   if currentLineArr.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     return currentLineArr
   end
